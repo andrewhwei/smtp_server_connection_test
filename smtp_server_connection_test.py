c015a94ad8f407a82c45b221f0a3f44ca405f455
Andrew Wei <awei@Varos-MacBook-Pro.local>
2016-11-28 11:28:30 -0800

    Initial commit

---
+++ smtp_server_connection_test.py
@@ -0,0 +1,16 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+msg = MIMEMultipart()
+msg['From'] = 'me@gmail.com'
+msg['To'] = 'you@gmail.com'
+msg['Subject'] = 'simple email in python'
+message = 'here is the email'
+msg.attach(MIMEText(message))
+
+mailserver = smtplib.SMTP('localhost', 1025)
+
+mailserver.sendmail('me@gmail.com', 'you@gmail.com', msg.as_string())
+
+mailserver.quit()
